ac4b7fb3265c740cc913a7137c86162827b4ce67
David Duthie <dave.a.duthie@gmail.com>
2017-05-10 00:20:15 +0800

    fix test

--- test/toe/ai_test.clj
+++ test/toe/ai_test.clj
@@ -8,7 +8,7 @@
    [:x :- :o]])
 
 (deftest best-move-test
-  (is (= ((negamax [:x :o] test-board-1 3 3) :board)
+  (is (= (best-move-for [:x :o] test-board-1 3 3)
          [[:x :o :o]
           [:x :x :-]
           [:x :- :o]])))
